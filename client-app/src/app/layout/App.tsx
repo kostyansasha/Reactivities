0fa742c7d3bb6243abe6b4a4f493491b5b688361
oleksandr.kostian <kostyansasha@ukr.net>
2023-02-20 15:55:40 +0100

    Section 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,75 @@
+import { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashbord from '../../features/activities/dashboard/ActivityDashboard';
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      console.log(response)
+      setActivities(response.data);
+    })
+  }, [])
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id
+      ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+      : setActivities([...activities, { ...activity, id: uuidv4() }]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(x => x.id !== id)]);
+  }
+
+  return (
+    //Fragment can be simplified as <>
+    <Fragment>
+
+      <NavBar openForm={handleFormOpen} />
+
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashbord
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+
+    </Fragment>
+  );
+}
+
+export default App;
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
-import App from './App';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import './app/layout/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
